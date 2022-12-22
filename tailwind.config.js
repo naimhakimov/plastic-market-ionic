203cff153fc228f6fe4f2f85de33507523b3c758
Naim <naimhakimov2001@gmail.com>
2022-12-22 23:01:20 +0500

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,8 @@ module.exports = {
       'gray-light': '#E6E6E6',
       'light-gray': "#F2F2F2",
       'gray-100': "#DADADA",
+      'gray-200': "#C0C0C0",
+      'gray-300': "#4D4D4D",
       'transparent': 'transparent',
       orange: '#EC5B22'
     },
