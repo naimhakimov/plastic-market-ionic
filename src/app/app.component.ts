5a1687bb0fa82d2a6d9d058df505b9760f073a91
Hakimov <naim.nanocoding@gmail.com>
2023-03-04 17:02:13 +0500

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { StatusBar, Style } from '@capacitor/status-bar'
+import { Platform } from '@ionic/angular'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  constructor(private platform: Platform) {
+    this.platform.ready().then(() => {
+      StatusBar.setStyle({ style: Style.Light })
+    })
+  }
+
+  ionViewWillEnter() {
+    StatusBar.setStyle({ style: Style.Light })
+  }
 
 }
